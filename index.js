417ed228d1d2c0b85f55d5cb18954bc57d90203c
Shaypher Ashe <shaypherashe@gmail.com>
2024-07-09 00:12:23 -0500

    updated index.html with new images

--- index.js
+++ index.js
@@ -36,6 +36,28 @@ const images = {
         storm: "resources/profile-images/storm-prof.png",
         ultron: "resources/profile-images/ultron-prof.png",
         vegeta: "resources/profile-images/vegeta-prof.png"
+    },
+    victory: {
+        allMight: "resources/winner-images/all-might-victory.png",
+        bane: "resources/winner-images/bane-victory.png",
+        captainAmerica: "resources/winner-images/captain-america-victory.png",
+        doctorDoom: "resources/winner-images/doctor-doom-victory.png",
+        donatello: "resources/winner-images/donatello-victory.png",
+        enchantress: "resources/winner-images/enchantress-victory.png",
+        harleyQuinn: "resources/winner-images/harley-quinn-victory.png",
+        kyloRen: "resources/winner-images/kylo-ren-victory.png",
+        loki: "resources/winner-images/loki-victory.png",
+        masterChief: "resources/winner-images/master-chief-victory.png",
+        moonKnight: "resources/winner-images/moon-knight-victory.png",
+        redHood: "resources/winner-images/red-hood-victory.png",
+        sauron: "resources/winner-images/sauron-victory.png",
+        scarletSpider: "resources/winner-images/scarlet-spider-victory.png",
+        spiderGwen: "resources/winner-images/spider-gwen-victory.png",
+        stockLeft: "resources/winner-images/stock-fighter-left-victory.png",
+        stockRight: "resources/winner-images/stock-fighter-right-victory.png",
+        storm: "resources/winner-images/storm-victory.png",
+        ultron: "resources/winner-images/ultron-victory.png",
+        vegeta: "resources/winner-images/vegeta-victory.png"
     }
 };
 
